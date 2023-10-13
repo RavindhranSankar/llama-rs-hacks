43ef3e035d90673ddb22541dee725821f3f05d69
Ravindhran Sankar <sravindhran.ganapathy@gmail.com>
2023-10-13 16:58:25 -0700

    new test

--- test_soap_notes.py
+++ test_soap_notes.py
@@ -8,6 +8,23 @@ import fire
 from llama import Llama, Dialog
 import os
 
+model = "LLAMA2_7B"
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+transcript_folder = "/home/ubuntu/daily/llama-rs-hacks/transcript_10-04_10"
+soap_folder = transcript_folder.replace("transcript", "soap")
+soap_folder = model + "_" + soap_folder
+
+transcript_path = os.path.join(current_directory, transcript_folder)
+soap_path = os.path.join(current_directory, soap_folder)
+
+# Create soap notes folder if needed
+os.makedirs(soap_folder, exist_ok=True)
+
+
+file_list = os.listdir(transcript_path)
+transcripts = [file for file in file_list if file.startswith("tr_") and file.endswith(".txt")]
+
 transcript_path = "/home/ubuntu/daily/llama-rs-hacks/transcript_10-04_10"
 txt_file = "tr_0_688w.txt"
 
@@ -15,21 +32,11 @@ system_context = """
 You are a nurse practitioner with over 20 years of experience
 writing clear, concise, and accurate SOAP notes for doctors. 
 Format the SOAP note into ONLY these four sections: 
-Subjective, Objective, Assessment, Plan and you list at 
-least two statements, but no more than 8 statements, per 
-section. Only include facts about the patient's symptoms 
-exactly as they are stated in a given transcript 'text'
-between doctor and patient in the Objective section.
+Subjective, Objective, Assessment, Plan. Only include facts 
+about the patient's symptoms exactly as they are stated in a 
+given transcript 'text' between doctor and patient in the 
+Objective section.
 """
-
-print(f"Reading {txt_file}:")
-transcript = None
-with open(os.path.join(transcript_path, txt_file), "r") as file:
-    transcript = file.read()
-
-prompt = f"Transcript: `{transcript}`"
-print("done. prompt ready....")
-print(prompt)
 
 
 def main(
@@ -63,25 +70,51 @@ def main(
         max_batch_size=max_batch_size,
     )
 
-    dialogs: List[Dialog] = [
-        [
-            {"role": "system", "content": system_context},
-            {"role": "user", "content": prompt},
-        ],
-    ]
+    for i, txt_file in enumerate(transcripts):
+        print(f"[{i+1}] : Reading {txt_file}:")
+        transcript = None
+        with open(os.path.join(transcript_path, txt_file), "r") as file:
+            transcript = file.read()
 
-    results = generator.chat_completion(
-        dialogs,  # type: ignore
-        max_gen_len=max_gen_len,
-        temperature=temperature,
-        top_p=top_p,
-    )
+        if not transcript:
+            print(f"Skipping {txt_file} ...")
+            continue
 
-    for dialog, result in zip(dialogs, results):
-        for msg in dialog:
-            print(f"{msg['role'].capitalize()}: {msg['content']}\n")
-        print(f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}")
-        print("\n==================================\n")
+        prompt = f"Transcript: `{transcript}`"
+        print("[{i+1}] : done. prompt ready....")
+
+        dialogs: List[Dialog] = [
+            [
+                {"role": "system", "content": system_context},
+                {"role": "user", "content": prompt},
+            ],
+        ]
+
+        print(f"[{i+1}] : sending query to {model}")
+        results = generator.chat_completion(
+            dialogs,  # type: ignore
+            max_gen_len=max_gen_len,
+            temperature=temperature,
+            top_p=top_p,
+        )
+
+        # for dialog, result in zip(dialogs, results):
+        #     for msg in dialog:
+        #         print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+        #     print(f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}")
+        #     print("\n==================================\n")
+
+        content = results[0]["generation"]["content"]
+        soap_file = txt_file.replace("tr_", "sp_")
+        soap_file_path = os.path.join(soap_folder, soap_file)
+
+        print(f"[{i+1}] : saving response")
+        # Open the file in write mode ('w')
+        with open(soap_file_path, "w") as file:
+            # Write the string to the file
+            file.write(content)
+
+        break
 
 
 if __name__ == "__main__":
